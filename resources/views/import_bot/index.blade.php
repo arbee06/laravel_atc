d6990d8c6a4847184d7b4369bedb5ff1c56b11c1
arbee06 <arbi0602@gmail.com>
2022-12-16 08:59:24 +0700

    'import_issue'

--- resources/views/import_bot/index.blade.php
+++ resources/views/import_bot/index.blade.php
@@ -34,7 +34,7 @@
                         <h5 class="modal-title" id="staticBackdropLabel">Import Form</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <form method="POST" action="{{ url('/home/import') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ url('/bot_by_import/import') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="modal-body">
                             <div class="row mb-3">
